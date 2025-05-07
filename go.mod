b13f05738d4387066a60b833a38c41ac1947d409
Caio Nogueira <caiodemelonogueira1@gmail.com>
2025-05-07 20:25:11 -0300

    função pra multiplicar slices

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nascimentops/multislices
+
+go 1.24.2
---
+++ math.go
@@ -0,0 +1,28 @@
+package multiarr
+
+//função que recebe duas slices e retorna a multiplicação de seus elementos
+func MultiElements(a, b []int) []int {
+	var lenght int
+
+	if len(a) > len(b) {
+		lenght = len(a)
+		for len(b) < lenght {
+			b = append(b, -1)
+		}
+	} else {
+		lenght = len(b)
+		for len(a) < lenght {
+			a = append(a, -1)
+		}
+	}
+
+	var slice []int = make([]int, lenght)
+
+	for i := 0; i < lenght; i++ {
+		if a[i] >= 0 && b[i] >= 0 {
+			slice[i] = a[i] * b[i]
+		}
+	}
+
+	return slice
+}
